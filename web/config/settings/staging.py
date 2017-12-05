5550a08d230d001d4e5d55e964a043a65bc6c88c
Paul Grau <paul@graycoding.com>
2017-12-05 21:00:08 +0900

    aws ses support

--- web/config/settings/staging.py
+++ web/config/settings/staging.py
@@ -2,6 +2,13 @@
 
 from .production import *  # noqa
 
-DEBUG = False
+DEBUG = True
 
 DEPLOYMENT = "staging"
+
+EMAIL_BACKEND = 'django_ses.SESBackend'
+AWS_SES_ACCESS_KEY_ID = env('AWS_SES_ACCESS_KEY_ID')
+AWS_SES_SECRET_ACCESS_KEY = env('AWS_SES_SECRET_ACCESS_KEY')
+AWS_SES_REGION_NAME = 'us-west-2'
+AWS_SES_REGION_ENDPOINT = 'email.us-west-2.amazonaws.com'
+DEFAULT_FROM_EMAIL = 'no-reply@api.budgetwiser.org'
