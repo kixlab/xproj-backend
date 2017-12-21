8fb96cbb9d4a1c47b9980204f594bdf604bd8763
Paul Grau <paul@graycoding.com>
2017-12-21 22:00:20 +0900

    update config for logging on server

--- web/config/settings/production.py
+++ web/config/settings/production.py
@@ -6,3 +6,46 @@ DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
 DEPLOYMENT = "production"
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] \
+%(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'WARNING',
+        },
+        'xproject': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'INFO',
+        },
+        'gensim.models': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'WARNING',
+        },
+        'root': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'WARNING',
+        },
+    }
+}
