3812a1f5536f6994d46a68649fc65eb403c4bd99
Ubuntu <ubuntu@ip-172-31-18-90.ap-northeast-2.compute.internal>
2018-08-16 07:02:22 +0000

    changed url

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-certbot -n certonly --standalone --force-renewal --preferred-challenges http -d xproj-api.hyunwoo.me --agree-tos --email grau@kaist.ac.kr
+certbot -n certonly --standalone --force-renewal --preferred-challenges http -d xproj-api.hyunwoo.me --agree-tos --email khw0726@kaist.ac.kr
