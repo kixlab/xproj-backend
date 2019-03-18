2f3f39e6b2da2492ff17366d0bb5cc8a7e5bf0ee
khw <khw0726@kaist.ac.kr>
2019-03-18 21:52:22 +0900

    qqqq

--- web/effect/taghelpers.py
+++ web/effect/taghelpers.py
@@ -61,82 +61,82 @@ class TagTree:
     def isEmpty(self):
         return len(self.included_tags) <= 0
 
-    # def construct_tag_tree(self, tag_list, policy):
-    #     sorted_tags = sorted(tag_list, key = lambda x: x[1], reverse = True)
-    #     tag_txt = [t[0] for t in sorted_tags]
-    #     self.compute_cooccur(tag_txt, policy)
-
-    #     for i in range(len(sorted_tags)):
-    #         level1_node = TagNode(sorted_tags[i][0], sorted_tags[i][2], sorted_tags[i][3])
-
-    #         for j in range(len(sorted_tags)):
-    #             if self.cooccur[i][j][0] + self.cooccur[i][j][1] > 0:
-    #                 level1_node.add_child_name(sorted_tags[j][0], self.cooccur[i][j][0], self.cooccur[i][j][1])
-
-    #         self.root.add_child(level1_node)
-
-    # def compute_cooccur(self, taglist, policy):
-    #     if self.cooccur is not None:
-    #         return
-
-    #     self.cooccur = [[[0,0] for t in taglist] for t in taglist]
-
-    #     queryset = Effect.objects.filter(is_guess = False).filter(policy = policy)
-
-    #     for e in queryset:
-    #         tags = e.tags.names()
-    #         for i in range(len(tags)):
-    #             for j in range(i):
-    #                 t1_idx = taglist.index(tags[i])
-    #                 t2_idx = taglist.index(tags[j])
-
-    #                 if e.isBenefit:
-    #                     self.cooccur[t1_idx][t2_idx][0] += 1
-    #                     self.cooccur[t2_idx][t1_idx][0] += 1
-    #                 else:
-    #                     self.cooccur[t1_idx][t2_idx][1] += 1
-    #                     self.cooccur[t2_idx][t1_idx][1] += 1
-        
-
     def construct_tag_tree(self, tag_list, policy):
         sorted_tags = sorted(tag_list, key = lambda x: x[1], reverse = True)
-        while len(sorted_tags) > 0:
-            tags_list = [x[0] for x in sorted_tags]
-            ele = sorted_tags.pop(0) # pick the most referenced one
-            queryset_level1 = Effect.objects.filter(tags__name__in=[ele[0]]).filter(is_guess = False)
-            level1_node = TagNode(ele[0], ele[2], ele[3])
-            self.included_tags.append(ele[0])
-            # self.root.add_child(level1_node)
+        tag_txt = [t[0] for t in sorted_tags]
+        self.compute_cooccur(tag_txt, policy)
 
-            # possible_children = list(queryset_level1.values('tags__name')) # extract possible childs
-            # possible_children_text = list(set([tag for tag in possible_children['tags__name']]))
-            # level1_node.pc = possible_children
-            for t in tags_list:
-                if t in self.included_tags:
-                    continue
-                
-                t12 = queryset_level1.filter(tags__name__in=[t])
-                t12_count = t12.count()
-                # t12_pos_count = t12.filter(isBenefit = True).count()
-                # t12_neg_count = t12.filter(isBenefit = False).count()
+        for i in range(len(sorted_tags)):
+            level1_node = TagNode(sorted_tags[i][0], sorted_tags[i][2], sorted_tags[i][3])
 
-                # if (t12_neg_count + t12_pos_count) > 0:
-                #     level1_node.add_child_name(t, t12_pos_count, t12_neg_count)
-                t2 = None
-                t2idx = None
-                for idx, tag in enumerate(sorted_tags):
-                    if tag[0] == t:
-                        t2idx = idx
-                        t2 = tag 
-                if t2 is None:
-                    continue
-
-                t2_count = t2[1]
-                if t12_count >= 0.85 * t2_count:
-                    level1_node.add_child_name(t2[0], t2[2], t2[3])
-                    sorted_tags.pop(t2idx)
+            for j in range(len(sorted_tags)):
+                if self.cooccur[i][j][0] + self.cooccur[i][j][1] > 0:
+                    level1_node.add_child_name(sorted_tags[j][0], self.cooccur[i][j][0], self.cooccur[i][j][1])
 
             self.root.add_child(level1_node)
+
+    def compute_cooccur(self, taglist, policy):
+        if self.cooccur is not None:
+            return
+
+        self.cooccur = [[[0,0] for t in taglist] for t in taglist]
+
+        queryset = Effect.objects.filter(is_guess = False).filter(policy = policy)
+
+        for e in queryset:
+            tags = e.tags.names()
+            for i in range(len(tags)):
+                for j in range(i):
+                    t1_idx = taglist.index(tags[i])
+                    t2_idx = taglist.index(tags[j])
+
+                    if e.isBenefit:
+                        self.cooccur[t1_idx][t2_idx][0] += 1
+                        self.cooccur[t2_idx][t1_idx][0] += 1
+                    else:
+                        self.cooccur[t1_idx][t2_idx][1] += 1
+                        self.cooccur[t2_idx][t1_idx][1] += 1
+        
+
+    # def construct_tag_tree(self, tag_list, policy):
+    #     sorted_tags = sorted(tag_list, key = lambda x: x[1], reverse = True)
+    #     while len(sorted_tags) > 0:
+    #         tags_list = [x[0] for x in sorted_tags]
+    #         ele = sorted_tags.pop(0) # pick the most referenced one
+    #         queryset_level1 = Effect.objects.filter(tags__name__in=[ele[0]]).filter(is_guess = False)
+    #         level1_node = TagNode(ele[0], ele[2], ele[3])
+    #         self.included_tags.append(ele[0])
+    #         # self.root.add_child(level1_node)
+
+    #         # possible_children = list(queryset_level1.values('tags__name')) # extract possible childs
+    #         # possible_children_text = list(set([tag for tag in possible_children['tags__name']]))
+    #         # level1_node.pc = possible_children
+    #         for t in tags_list:
+    #             if t in self.included_tags:
+    #                 continue
+                
+    #             t12 = queryset_level1.filter(tags__name__in=[t])
+    #             t12_count = t12.count()
+    #             # t12_pos_count = t12.filter(isBenefit = True).count()
+    #             # t12_neg_count = t12.filter(isBenefit = False).count()
+
+    #             # if (t12_neg_count + t12_pos_count) > 0:
+    #             #     level1_node.add_child_name(t, t12_pos_count, t12_neg_count)
+    #             t2 = None
+    #             t2idx = None
+    #             for idx, tag in enumerate(sorted_tags):
+    #                 if tag[0] == t:
+    #                     t2idx = idx
+    #                     t2 = tag 
+    #             if t2 is None:
+    #                 continue
+
+    #             t2_count = t2[1]
+    #             if t12_count >= 0.85 * t2_count:
+    #                 level1_node.add_child_name(t2[0], t2[2], t2[3])
+    #                 sorted_tags.pop(t2idx)
+
+    #         self.root.add_child(level1_node)
 
 def decide_inclusion(tag1, tag2):
     queryset = Effect.objects.all()
