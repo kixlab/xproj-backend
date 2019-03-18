9667dc36140a70e32eec448d3971354817a99052
khw <khw0726@kaist.ac.kr>
2019-03-18 20:44:35 +0900

    self

--- web/effect/taghelpers.py
+++ web/effect/taghelpers.py
@@ -51,10 +51,12 @@ class TagNode:
 class TagTree:
     root = None
     included_tags = []
+    cooccur = None
 
     def __init__(self):
         self.root = TagNode('root')
         self.included_tags = []
+        self.cooccur = None
 
     def isEmpty(self):
         return len(self.included_tags) <= 0
@@ -62,19 +64,22 @@ class TagTree:
     def construct_tag_tree(self, tag_list, policy):
         sorted_tags = sorted(tag_list, key = lambda x: x[1], reverse = True)
         tag_txt = [t[0] for t in tag_list]
-        cooccur = compute_cooccur(tag_txt, policy)
+        self.compute_cooccur(tag_txt, policy)
 
         for i in range(sorted_tags):
             level1_node = TagNode(tag_list[0], tag_list[2], tag_list[3])
 
             for j in range(sorted_tags):
-                if cooccur[i][j][0] + cooccur[i][j][1] > 0:
-                    level1_node.add_child_name(sorted_tags[j], cooccur[i][j][0], cooccur[i][j][1])
+                if self.cooccur[i][j][0] + self.cooccur[i][j][1] > 0:
+                    level1_node.add_child_name(sorted_tags[j], self.cooccur[i][j][0], self.cooccur[i][j][1])
 
             self.root.add_child(level1_node)
 
     def compute_cooccur(self, taglist, policy):
-        cooccur = [[[0,0] for t in taglist] for t in taglist]
+        if self.cooccur is not None:
+            return
+
+        self.cooccur = [[[0,0] for t in taglist] for t in taglist]
 
         queryset = Effect.objects.filter(is_guess = False).filter(policy = policy)
 
@@ -85,14 +90,12 @@ class TagTree:
                     t2_idx = taglist.index(e.tags[j])
 
                     if e.isBenefit:
-                        cooccur[t1_idx][t2_idx][0] += 1
-                        cooccur[t2_idx][t1_idx][0] += 1
+                        self.cooccur[t1_idx][t2_idx][0] += 1
+                        self.cooccur[t2_idx][t1_idx][0] += 1
                     else:
-                        cooccur[t1_idx][t2_idx][1] += 1
-                        cooccur[t2_idx][t1_idx][1] += 1
+                        self.cooccur[t1_idx][t2_idx][1] += 1
+                        self.cooccur[t2_idx][t1_idx][1] += 1
         
-        return cooccur
-
 
 
         # while len(sorted_tags) > 0:
