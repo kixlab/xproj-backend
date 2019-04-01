576cb657d806dd4dd1685af65151b139183a131d
khw <khw0726@kaist.ac.kr>
2019-04-01 13:48:35 +0900

    fix div/0 error

--- web/effect/taghelpers.py
+++ web/effect/taghelpers.py
@@ -207,7 +207,7 @@ class TagCoOccur:
 
         # return self.tag_txt[target[0]]
 
-        cooccur_list = list(zip([x[0] for x in self.taglist], [x[1] for x in self.taglist], [abs(tag_ratio - (x[1]/ x[0] * 100)) for x in self.cooccur[tagidx]])) # txt, total_count, co_occur
+        cooccur_list = list(zip([x[0] for x in self.taglist], [x[1] for x in self.taglist], [abs(tag_ratio - (x[1]/ x[0] * 100)) if x[0] > 0 else -1 for x in self.cooccur[tagidx] ])) # txt, total_count, co_occur
 
         cooccur_list.sort(key = lambda x: x[2]) # the least co-occuring tag with the largest total occurance, so the users can discover unknown groups 
 
